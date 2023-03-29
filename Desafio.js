e6572d522eef362565bc8ffb1a84fba7cabcbf6e
Noelia Lavilla <noelia.m.lavilla@hotmail.com>
2023-03-29 14:03:24 -0300

    Se mejora codigo

--- Desafio.js
+++ Desafio.js
@@ -1,38 +1,46 @@
 
-
 class ProductManager {
-    constructor(){
-        this.products = [];
-    }
     constructor(products){
         this.products = products;
         
     }
 
-    addProduct = (title, description, price, thumbnail, code, stock) => {
-        prod = Product(title, description, price, thumbnail, code, stock)
+    static id = 0;
 
+    addProduct = (title, description, price, thumbnail, code, stock) => {
+        var prod = new Product(title, description, price, thumbnail, code, stock);
+        for (var i = 0; i < this.products.length; i++) {
+            if (this.products[i].code == prod.code) {
+                console.log(`No se puede añadir producto ${prod.title}, code repetido.`);
+                return;
+            }
+        }
         this.products.push(prod);
-    }
+        ProductManager.id++;
+
+
+
+    };
     getProducts = () => {
         return this.products;
-    }
+    };
 
-    
+
 
 
 }
 
-class Product{
+class Product {
 
-    constructor(title, description, price, thumbnail, code, stock){
-        this.title =title;
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.title = title;
         this.description = description;
         this.price = price;
         this.thumbnail = thumbnail;
         this.code = code;
         this.stock = stock;
-         
+        this.id = ProductManager.id;
+
     }
 
 }
