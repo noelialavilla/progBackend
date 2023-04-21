b505b8978df430eec58856cc62138e422baecddc
Noelia Lavilla <noelia.m.lavilla@hotmail.com>
2023-04-20 23:18:18 -0300

    Se sube desafio final

--- Desafio/src/app.js
+++ Desafio/src/app.js
@@ -3,31 +3,30 @@ import ProductManager from '../managers/ProductManager.js';
 
 const app = express();
 
+app.use(express.json());
+
 const productManager = new ProductManager('./files/products.json');
 
 app.use(express.urlencoded({ extended: true }));
 
-
-
-//Ruta /products/:pid tipo app.get donde debemos llamar al metodo getById de la clae ProductManager usar la instancia de la clase ya creada
-
-//Ejmplo de async await llamado a la clase ProductMnager
 app.get('/products', async (req, res) => {
 
     const { limit } = req.query;
     if (limit) {
         const products = await productManager.getProducts(limit);
-        res.send({ products });
+        res.status(200).send(json(products));
+        
     } else {
         const products = await productManager.getProducts(0);
-        res.send({ products });
+        res.status(200).send(json(products));
+
     }
 
 
 })
 app.get('/products/:pid', async (req, res) => {
     const pid = Number(req.params.pid);
-    res.send(await productManager.getProductById(pid));
+    res.status(200).send(json(await productManager.getProductById(pid)));
 })
 
 app.listen(8080, () => console.log("Listening on 8080"))
